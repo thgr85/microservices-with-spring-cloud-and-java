b497b5d78f58b3eec0baeea2492b0b7a8150a1b3
Thiago Oliveira <thgr85@outlook.com>
2025-11-02 15:47:00 -0300

    feature: handle bad requests

---
+++ rest-with-spring-boot-and-java/src/main/java/br/com/thgr/controllers/MathController.java
@@ -0,0 +1,30 @@
+package br.com.thgr.controllers;
+
+import br.com.thgr.exception.UnsupportedMathOperationException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw new UnsupportedMathOperationException("Please set a numeric value");
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) {
+        if (strNumber == null || strNumber.isEmpty()) throw new UnsupportedMathOperationException("Please set a numeric value");
+        String number = strNumber.replace(',', '.');
+        return Double.parseDouble(number);
+    }
+
+    private boolean isNumeric(String strNumber) {
+        if (strNumber == null || strNumber.isEmpty()) return false;
+        String number = strNumber.replace(',', '.');
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+}
